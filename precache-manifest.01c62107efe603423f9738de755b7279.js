e940a99807090aca12049620a5510572e8ca24cf
w1zzzhashtag <v.kuzmin.w1zzz@gmail.com>
2020-07-12 15:46:38 +0400

    Updates

---
+++ precache-manifest.01c62107efe603423f9738de755b7279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87a29d3b4f73964d70e4ec5744c4c93",
+    "url": "/simtech-test-dev/index.html"
+  },
+  {
+    "revision": "72a0ad747ed70d64eb8d",
+    "url": "/simtech-test-dev/static/css/main.50a4e5e0.chunk.css"
+  },
+  {
+    "revision": "bdffc18cae2ea78aa582",
+    "url": "/simtech-test-dev/static/js/2.48320072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simtech-test-dev/static/js/2.48320072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a0ad747ed70d64eb8d",
+    "url": "/simtech-test-dev/static/js/main.9b5dbd64.chunk.js"
+  },
+  {
+    "revision": "9da9ca7409cb71180cc8",
+    "url": "/simtech-test-dev/static/js/runtime-main.b9f1b20c.js"
+  },
+  {
+    "revision": "0fcb68ef20692679b6c17fb902e1896e",
+    "url": "/simtech-test-dev/static/media/CVV.0fcb68ef.png"
+  }
+]);
